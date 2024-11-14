288578a0d98d1a90fc3a7d2bc0deb61f54353b43
kailahgayle <kailahkg@gmail.com>
2024-11-14 11:06:01 -0500

    added sandwich class with getters and setters

---
+++ DELIcious/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,48 @@
+package com.pluralsight;
+
+
+import java.util.Scanner;
+
+/* public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        showHomeScreen(scanner);//Where home screen starts
+    }
+
+    //Home Screen Options
+    private static void showHomeScreen(Scanner scanner) {
+        while (true) { //make sure app stays open until user exits
+            System.out.println("-------- WELCOME TO OUR DELI --------");
+            System.out.println("N) New Order");
+            System.out.println("X) Exit");
+            System.out.println("Please Enter a Choice: ");
+
+            // Prompt for user input
+            String choice = scanner.nextLine().toUpperCase();
+
+            //Results after user input
+            switch (choice) {
+                case "N":
+                    //startNewOrder(scanner);
+                    break;
+                case "X":
+                    System.out.println("Exiting..");
+                    scanner.close();
+                    return; //this will exit program
+                default:
+                    System.out.println("Invalid option. Please try Again.");
+                    break; // this added scanner would let you stay in home screen if it's an invalid option
+
+            }
+        }
+    }
+
+   /* private static void startNewOrder(Scanner scanner) {
+
+     Method for N)New Order
+        Order order = new Order();
+        showOrderScreen(scanner, order);
+    }
+
+    */
+}
---
+++ DELIcious/src/main/java/com/pluralsight/Sandwich.java
@@ -0,0 +1,83 @@
+package com.pluralsight;
+
+import java.util.List;
+
+public class Sandwich {
+    private String size;
+    private String breadType;
+    private List<String> meats;
+    private List<String> cheeses;
+    private List<String> toppings;
+    private List<String> sauces;
+    private boolean toasted;
+
+    public Sandwich(String size, String breadType, List<String> meats, List<String> cheeses, List<String> toppings, List<String> sauces, boolean toasted) {
+        this.size = size;
+        this.breadType = breadType;
+        this.meats = meats;
+        this.cheeses = cheeses;
+        this.toppings = toppings;
+        this.sauces = sauces;
+        this.toasted = toasted;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getBreadType() {
+        return breadType;
+    }
+
+    public void setBreadType(String breadType) {
+        this.breadType = breadType;
+    }
+
+    public List<String> getMeats() {
+        return meats;
+    }
+
+    public void setMeats(List<String> meats) {
+        this.meats = meats;
+    }
+
+    public List<String> getCheeses() {
+        return cheeses;
+    }
+
+    public void setCheeses(List<String> cheeses) {
+        this.cheeses = cheeses;
+    }
+
+    public List<String> getToppings() {
+        return toppings;
+    }
+
+    public void setToppings(List<String> toppings) {
+        this.toppings = toppings;
+    }
+
+    public List<String> getSauces() {
+        return sauces;
+    }
+
+    public void setSauces(List<String> sauces) {
+        this.sauces = sauces;
+    }
+
+    public boolean isToasted() {
+        return toasted;
+    }
+
+    public void setToasted(boolean toasted) {
+        this.toasted = toasted;
+    }
+    // This method will calculate the price of the sandwich
+    public double calculatePrice() {
+        return 0.0;
+    }
+}
