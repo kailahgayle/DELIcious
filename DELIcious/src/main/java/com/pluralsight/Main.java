ad59e0a7a67ba0b85649d7ddb11aa4f53720ac3d
kailahgayle <kailahkg@gmail.com>
2024-11-14 23:02:42 -0500

    Initial Home screen updates

--- DELIcious/src/main/java/com/pluralsight/Main.java
+++ DELIcious/src/main/java/com/pluralsight/Main.java
@@ -3,46 +3,55 @@ package com.pluralsight;
 
 import java.util.Scanner;
 
- public class Main {
+
+
+public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        showHomeScreen(scanner);//Where home screen starts
-    }
 
-    //Home Screen Options
-    private static void showHomeScreen(Scanner scanner) {
-        while (true) { //make sure app stays open until user exits
-            System.out.println("-------- WELCOME TO OUR DELI --------");
-            System.out.println("N) New Order");
-            System.out.println("X) Exit");
-            System.out.println("Please Enter a Choice: ");
 
-            // Prompt for user input
-            String choice = scanner.nextLine().toUpperCase();
 
-            //Results after user input
+        while (true) {
+
+            System.out.println("======WELCOME TO OUR DELI-CIOUS DELI======");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+
+            int choice = scanner.nextInt();
+
+            scanner.nextLine(); // Newline
+
+
+
             switch (choice) {
-                case "N":
-                    //startNewOrder(scanner);
+                case 1:
+                    System.out.println("Starting new order...");
+                    startNewOrder(scanner); // Placeholder
                     break;
-                case "X":
-                    System.out.println("Exiting..");
-                    scanner.close();
-                    return; //this will exit program
-                default:
-                    System.out.println("Invalid option. Please try Again.");
-                    break; // this added scanner would let you stay in home screen if it's an invalid option
 
+                case 0:
+                    System.out.println("======THANK YOU, SEE YOU NEXT TIME!======");
+                    break; // will exit loop
+
+                default:
+                    System.out.println("Invalid option. Please enter '1' to start a new order or '0' to exit.");
+            }
+
+            if (choice == 0) {
+                break; // Exits the loop when the user selects "0"
             }
         }
+
+
+
+        scanner.close();
     }
 
-   /* private static void startNewOrder(Scanner scanner) {
 
-     Method for N)New Order
-        Order order = new Order();
-        showOrderScreen(scanner, order);
+    public static void startNewOrder(Scanner scanner) {
+        System.out.println("Order Screen Loading...");
+        // for order screen
     }
-
-    */
 }
